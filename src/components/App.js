3950a043a6bc6a90dccae09b2fbd38a101db9566
Rahul Passi <rahulpassi2412@gmail.com>
2021-06-24 21:50:21 +0530

    hook api call added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [customers, setCustomers] = useState(0);
+
+  useEffect(() => {
+    axios.get('https://intense-tor-76305.herokuapp.com/merchants')
+      .then(response => {
+        setCustomers(response.data)
+      }).catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-    To Do ...
+    <div>
+      {customers.length}
     </div>
   );
 }
 
-export default App;
+export default App
